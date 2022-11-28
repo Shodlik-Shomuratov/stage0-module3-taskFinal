b508ac50cdde15eb71a118738f699dc337c53588
Shodlik Shomuratov <shodlikcoder@gmail.com>
2022-11-28 17:21:30 +0500

    All tasks done!

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int first = number / 100;
+        int second = number / 10 % 10;
+        int third = number % 10;
+
+        System.out.print(first);
+        System.out.print(second);
+        System.out.println(third);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,5 +2,8 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
+        double fahrenheit = (temperatureCelsius * 1.0 * 9 / 5) + 32;
+
+        System.out.println(fahrenheit);
     }
 }
